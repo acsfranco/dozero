0bee54d191c6ba97554093d2ac709170b4e53b52
alexandrecsfranco <acsfranco@ifba.edu.br>
2025-12-28 11:10:20 -0300

    add comments to perceptron.c

--- redesneurais/src/perceptron.c
+++ redesneurais/src/perceptron.c
@@ -1,3 +1,16 @@
+/*
+ * perceptron.c
+ *
+ * Implementação de um perceptron,
+ * sem uso de bibliotecas externas.
+ *
+ * Este arquivo contém:
+ * - definição da estrutura do perceptron
+ * - função de custo e computação do neurônio
+ *
+ * Objetivo educacional: mostrar como tudo funciona "por baixo".
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -6,6 +19,10 @@
 
 typedef struct Neuron NEURON;
 
+/* =======================
+ * Estrutura do perceptron
+ * ======================= */
+
 struct Neuron {
   float *weights;
   uint32_t nconnections;
@@ -13,9 +30,30 @@ struct Neuron {
   float (*actfunc)(float x);
 };
 
+/*
+ * Calcula a função de custo Identidade.
+ *
+ * Parâmetros:
+ *   x - Um valor escalar
+ *
+ * Retorno:
+ *   valor do custo da rede neural
+ */
+
 float ident(float x) {
   return x;
 }
+
+/*
+ * Computa o valor de saída do neurônio.
+ *
+ * Parâmetros:
+ *   neuron - neurônio a ser computado
+ *   x - vetor de entrada
+ *
+ * Retorno:
+ *   Valor de saída do neurônio
+ */
 
 float computout(NEURON neuron, float *x) {
   float k = 0;
@@ -28,10 +66,32 @@ float computout(NEURON neuron, float *x) {
   return neuron.actfunc(k);
 }
 
+/*
+ * Escolhe um valor randômico entre dois valores
+ *
+ * Parâmetros:
+ *   min - valor minimo a ser escolhido
+ *   max - valor máximo a ser escolhido
+ *
+ * Retorno
+ *   Um valor randômico entre min e max 
+ */
+
 float randomize(float min, float max) {
   float num = min + ((float)rand() / (float)RAND_MAX) * (max - min);
   return num;
 }
+
+/*
+ * Cria um neurônio, inicializa seu pesos e bia
+ *
+ * Parâmetros:
+ *   actfunc - a função de ativação do neurônio
+ *   nconnections - número de conexões do neurônio
+ *
+ * Retorno
+ *   O neurônio criado.
+ */
 
 NEURON initneuron(float (*actfunc)(float x), uint32_t nconnections) {
   NEURON neuron;
